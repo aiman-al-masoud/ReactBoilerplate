758048990d392f5d228249b9851082ea2726a206
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-03-09 09:15:00 +0100

    ts works

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+
+  entry: path.join(__dirname, "app", "src", "index.tsx"),
+
+  module: {
+
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+
+
+
+        
+      },
+    ],
+  },
+
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+
+
+  output: {
+    filename: 'bundle.js',
+    path:path.resolve(__dirname, "dist"),
+  },
+
+
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.(png|jpg|gif|wav|mp3)$/i,
+  //       use: [
+  //         {
+  //           loader: 'url-loader',
+  //           options: {
+  //             limit: 1000000000,
+  //           },
+  //         },
+  //       ],
+  //     },
+
+
+  //     {
+  //       test: /\.css$/i,
+  //       use: ["style-loader", "css-loader"],
+  //     },
+
+      
+  //     {
+  //       test: /\.?jsx?$/,
+  //       exclude: /node_modules/,
+  //       use: {
+  //         loader: "babel-loader",
+  //         options: {
+  //           presets: ['@babel/preset-env', '@babel/preset-react'],
+  //           plugins :['@babel/plugin-transform-runtime'] 
+  //         }
+  //       }
+  //     }
+
+  //   ]
+  // },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "app","src", "index.html"),
+    }),  
+  ],
+
+
+}
