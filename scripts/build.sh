112a6cd2f56ccb10b570ae6c8b6083d9dfc1b659
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-03-09 13:31:40 +0100

    fixed script removing unnecessary dist files

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 webpack  
 echo "<script>" >> ./dist/index.html  
-cat ./dist/main.js >> ./dist/index.html  
+cat ./dist/bundle.js >> ./dist/index.html  
 echo "</script>" >> ./dist/index.html  
-sed -i 's/<script defer="defer" src="main.js">//g' ./dist/index.html   
-rm ./dist/main.js
-rm ./dist/main.js.LICENSE.txt
+sed -i 's/<script defer="defer" src="bundle.js">//g' ./dist/index.html   
+rm ./dist/bundle.js
+rm ./dist/bundle.js.LICENSE.txt
