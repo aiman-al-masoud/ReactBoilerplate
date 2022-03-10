3d23d1a87436129fe40625d7a7f8003347d0d872
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-03-10 08:12:10 +0100

    playing example audio with audio tag

--- app/src/TestClass.tsx
+++ app/src/TestClass.tsx
@@ -1,18 +1,22 @@
 import React from "react"
-import {  Component, ReactNode } from "react";
+import { Component, ReactNode } from "react";
 import LeafImage from "../res/leaf.png"
 import CorrectSound from "../res/correct.mp3"
 
 
-export default class TestClass extends Component{
-    constructor(props:any){
+export default class TestClass extends Component {
+    constructor(props: any) {
         super(props)
     }
 
     render(): ReactNode {
-        return  (<div>
+        return (<div>
             <h1>Ciao Mondo</h1>
-            <img src={LeafImage} alt="" />
+            <audio controls>
+                <source src={CorrectSound} />
+                Sorry your browser doesn't support the audio tag
+            </audio>
+            <img src={LeafImage} alt="" width="200" />
         </div>)
     }
 
--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,8 +1,7 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import LeafImage from "../res/leaf.png"
-import CorrectSound from "../res/correct.mp3"
 import TestClass from "./TestClass"
+
 
 
 ReactDOM.render( <TestClass/> , document.getElementById("root"))
