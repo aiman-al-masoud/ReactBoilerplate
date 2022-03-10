96f66d31ae9d89704c2705285fe0e52b3f0c394d
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-03-10 08:55:39 +0100

    added cors api for async fetch example

--- app/src/TestClass.tsx
+++ app/src/TestClass.tsx
@@ -4,9 +4,23 @@ import LeafImage from "../res/leaf.png"
 import CorrectSound from "../res/correct.mp3"
 
 
-export default class TestClass extends Component {
+
+export default class TestClass extends Component<any, any> {
     constructor(props: any) {
         super(props)
+        this.state = {
+            responseOk : false
+        }
+    }
+
+    backgroundTask = async ()=>{
+        try{
+            let res = await fetch("http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=astro&output=xml")
+            console.log(res)
+            this.setState({responseOk : res.ok})
+        }catch{
+            this.setState({responseOk : false})
+        }
     }
 
     render(): ReactNode {
@@ -17,6 +31,8 @@ export default class TestClass extends Component {
                 Sorry your browser doesn't support the audio tag
             </audio>
             <img src={LeafImage} alt="" width="200" />
+            <button onClick={this.backgroundTask}>Ping Server</button>
+            <p>{this.state.responseOk? "server responded" : "server didn't respond"}</p>
         </div>)
     }
 
